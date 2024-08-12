f3751b14293f4134b6d1f26ca49fc895cf839815
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2024-08-12 16:48:49 +0900

    axe-core で指摘された箇所を修正

--- app.vue
+++ app.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-import AppHeader from '~/components/AppHeader.vue';
-import AppFooter from '~/components/AppFooter.vue';
-
-const config = useRuntimeConfig();
+import AppHeader from "~/components/AppHeader.vue";
+import AppFooter from "~/components/AppFooter.vue";
 </script>
 
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
     <AppHeader />
-    <NuxtPage />
+    <main>
+      <NuxtPage />
+    </main>
     <AppFooter />
-    <VueAxePopup v-if="$config.public.env === 'development'" />
   </NuxtLayout>
 </template>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,23 +2,9 @@
   <header>
     <nav>
       <ul>
-        <li><NuxtLink to="/">トップページ</NuxtLink></li>
-        <li><NuxtLink to="/badA11y">バッドアクセシビリティ</NuxtLink></li>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
       </ul>
     </nav>
   </header>
 </template>
-
-<style scoped>
-header {
-  background-color: #f0f0f0;
-  padding: 1rem;
-}
-
-nav ul {
-  list-style-type: none;
-  padding: 0;
-  display: flex;
-  gap: 1rem;
-}
-</style>
